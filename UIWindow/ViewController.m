17af4cab89f505c4b67c7d7e77d53212342c8d37
deliangyang <yangdeliang@autohello.com>
2015-09-20 13:58:09 +0800

    ui image view :lol:

--- UIWindow/ViewController.m
+++ UIWindow/ViewController.m
@@ -19,11 +19,39 @@
     
     UIView *view = [[UIView alloc] init];
     // set size
-    view.frame   = CGRectMake(100, 100, 50, 50);
+    view.frame   = CGRectMake(10, 30, 355, 627);
     // set background color
     view.backgroundColor = [UIColor redColor];
     // add view into parent view
     [self.view addSubview:view];
+    
+    NSLog(@"width:%f, height:%f", [[UIScreen mainScreen] bounds].size.width, [[UIScreen mainScreen] bounds].size.height);
+    
+    UILabel *label = [[UILabel alloc] init];
+    label.frame = CGRectMake(10, 100, 300, 30);
+    label.backgroundColor = [UIColor yellowColor];
+    // label add text
+    label.text = @"Hello world";
+    // text center
+    label.textAlignment = NSTextAlignmentCenter;
+    
+    // user define color
+    //opacity clearColor
+    label.textColor = [UIColor colorWithRed:0.3 green:0.4 blue:0.4 alpha:0.4];
+    
+    [self.view addSubview:label];
+    
+    // image view
+    NSString *path = [[NSBundle mainBundle] resourcePath];
+    NSString *imagePath = [NSString stringWithFormat:@"%@/bbb.jpg", path];
+    
+    UIImage *image = [[UIImage alloc] initWithContentsOfFile:imagePath];
+    
+    // need carrier
+    UIImageView *imageView = [[UIImageView alloc] initWithImage:image];
+    imageView.frame = CGRectMake(10, 10, 300, 300);
+    
+    [self.view addSubview:imageView];
     
     // Do any additional setup after loading the view, typically from a nib.
 }
